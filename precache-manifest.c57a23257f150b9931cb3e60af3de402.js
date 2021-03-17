fe7cf13edaa024cd8cf400aa5d5d390f385acb51
praveend <praveend2622@gmail.com>
2021-03-17 21:49:06 +0530

    Updates

---
+++ precache-manifest.c57a23257f150b9931cb3e60af3de402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e739f878b208e5066b8e3a6efff4e8d8",
+    "url": "/tiktok_frontend/index.html"
+  },
+  {
+    "revision": "20fc8432fe6997679964",
+    "url": "/tiktok_frontend/static/css/main.37ec7b1a.chunk.css"
+  },
+  {
+    "revision": "9d3be65f060d3919aba6",
+    "url": "/tiktok_frontend/static/js/2.44717aab.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tiktok_frontend/static/js/2.44717aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20fc8432fe6997679964",
+    "url": "/tiktok_frontend/static/js/main.e10601d0.chunk.js"
+  },
+  {
+    "revision": "caff364e5f4f73dcb176",
+    "url": "/tiktok_frontend/static/js/runtime-main.141fc107.js"
+  }
+]);
